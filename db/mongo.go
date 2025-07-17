ecd136c6a3f919412dae4a0c29d3af2c8e069087
amanagarwal3 <105861536+amanagarwal3@users.noreply.github.com>
2025-07-17 13:17:28 +0530

    Create mongo.go

---
+++ db/mongo.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Client *mongo.Client
+var ProvisionCollection *mongo.Collection
+
+func Connect() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal("MongoDB connection error:", err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("MongoDB ping error:", err)
+	}
+
+	Client = client
+	ProvisionCollection = client.Database("mini_poa").Collection("requests")
+	fmt.Println("✅ Connected to MongoDB")
+}
